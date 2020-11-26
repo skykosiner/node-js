c7c69924619fccbea42b85fb5123bff0af9cbc6d
yonikosiner <yoni@kosiner.co.uk>
2020-11-26 13:18:15 +0000

    http over now using express

--- server.js
+++ server.js
@@ -5,16 +5,36 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
   // Set header content type
   res.setHeader("Content-Type", "text/html");
   // Send html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
     } else {
-      res.write(data);
-      res.end();
+      res.end(data);
     }
   });
 });
