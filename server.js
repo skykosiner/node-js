744814f4db2c101f6ebd9e08fe6c94ee27372e94
yonikosiner <yoni@kosiner.co.uk>
2020-11-26 12:23:52 +0000

    install commit

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // Set header content type
+  res.setHeader("Content-Type", "text/html");
+  // Send html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
